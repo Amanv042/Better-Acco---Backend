c4e7fc6009cf21ffdde220e6a38afbd9486f9c81
Aman <amanverma0428@gmail.com>
2024-08-06 14:10:59 +0530

    added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from "mongoose";
+
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema(
+  {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, require: [true, "Password is required"] },
+    avatar: {
+      type: String,
+      require: true,
+    },
+    imagePath: {
+      type: String,
+    },
+    favoriteList: [{ type: Schema.Types.ObjectId, ref: "Hotel" }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.mothods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
